f53a3210b1e9662edfdf4a086c0d23ad814e6dcf
anna-107 <anonymousisgod61@gmail.com>
2025-08-14 17:46:39 +0530

    Commit from Ghost CodeShare

---
+++ java1.java
@@ -0,0 +1,20 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+File name
+@Component
+public class SecurityCredentialsPrinter implements CommandLineRunner {
+Hello world  
+    @Value("${spring.security.user.name}")
+    private String username;
+
+    @Value("${spring.security.user.password}")
+    private String password;
+hey
+    @Override
+    public void run(String... args) {
+        System.out.println("🔐 Default Spring Security Credentials:");
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+}
